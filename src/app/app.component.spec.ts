02d55931ebd8302415a8cc19ad3d78b37797a273
nish308 <morenishant495@gmail.com>
2023-09-04 08:50:48 +0530

    crudapp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud');
+    // expect(app.title).toEqual('crud');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CoreService } from './core/core.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crud';
+export class AppComponent implements OnInit{
+  displayedColumns: string[] = [
+    'id',
+    'firstName', 
+    'lastName', 
+    'email', 
+    'dob', 
+    'gender', 
+    'education', 
+    'company', 
+    'experience', 
+    'package',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private _dialog: MatDialog, 
+    private _empService: EmployeeService,
+    private _coreService: CoreService
+    ) {}
+
+  ngOnInit(): void{
+    this.getEmployeeList();
+  }
+
+  openAddEditEmpForm(){
+    const dialogRef =  this._dialog.open(EmpAddEditComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if(val) {
+          this.getEmployeeList();
+        }
+      }
+    })
+  }
+
+  getEmployeeList(){
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteEmployee(id: number){
+    this._empService.deleteEmployee(id).subscribe({
+      next: (res) => {
+        this._coreService.openSnackBar('Employee Deleted!', 'done');
+        this.getEmployeeList();
+      },
+      error:console.log,
+    })
+  }
+
+  openEditForm(data: any){
+    const dialogRef = this._dialog.open(EmpAddEditComponent,{
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if(val) {
+          this.getEmployeeList();
+        }
+      }
+    })  
+  }
 }
+
+
+
